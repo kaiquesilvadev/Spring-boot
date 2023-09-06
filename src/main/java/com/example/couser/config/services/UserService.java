be3ad63f3079aea66e25b3536fbf3c4e29c3cbb6
kaiquesilvadev <j.kaique1570@gmail.com>
2023-09-06 20:46:09 -0300

    implementando camada services

---
+++ src/main/java/com/example/couser/config/services/UserService.java
@@ -0,0 +1,31 @@
+package com.example.couser.config.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.example.couser.entities.User;
+import com.example.couser.repositories.UseRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UseRepository useRepository;
+	
+	public List<User> findAll(){
+		return useRepository.findAll();
+	}
+	
+	public ResponseEntity<User> findBy(Long id) {
+	          return useRepository.findById(id)
+				.map(x -> ResponseEntity.ok(x))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+}
--- src/main/java/com/example/couser/resources/UserResource.java
+++ src/main/java/com/example/couser/resources/UserResource.java
@@ -3,12 +3,15 @@ package com.example.couser.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.couser.config.services.UserService;
 import com.example.couser.entities.User;
 import com.example.couser.repositories.UseRepository;
 
@@ -17,12 +20,19 @@ import com.example.couser.repositories.UseRepository;
 public class UserResource {
 	
 	@Autowired
+	private UserService userService;
+	
+	@Autowired
 	private UseRepository repository;
 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		return ResponseEntity.ok(repository.findAll());
-		
+		List<User> user = new UserService().findAll();
+		return ResponseEntity.ok().body(user);
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findBy(@PathVariable Long id) {
+	         return userService.findBy(id);
+	}
 }
